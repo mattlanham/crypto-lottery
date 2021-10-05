400cd2ff5d20bd16184bf86eb909c51a1ee6bf56
Matthew Lanham <matt@geckoengage.com>
2021-10-05 08:42:00 +0100

    Writing tests

--- test/CryptoLottery.js
+++ test/CryptoLottery.js
@@ -2,18 +2,52 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("CryptoLottery", function () {
-  it("Should return new price once changed", async function () {
-    const CryptoLottery = await ethers.getContractFactory("CryptoLottery");
-    const lottery = await CryptoLottery.deploy();
-    await lottery.deployed();
 
-    expect(await lottery.getTicketPrice()).to.equal(ethers.utils.formatEther('0.01'));
+  before(async function () {
+    this.CryptoLottery = await ethers.getContractFactory('CryptoLottery');
+    this.lottery = await this.CryptoLottery.deploy();
+    await this.lottery.deployed();
+  });
 
-    const setTicketPriceTx = await lottery.setTicketPrice("0.02");
+  it("Should have zero tickets initially", async function () {
+    expect(await this.lottery.getTickets()).lengthOf(0);
+  });
 
-    // wait until the transaction is mined
+  it("Should have zero winners initially", async function () {
+    expect(await this.lottery.getWinners()).lengthOf(0);
+  });
+
+  it("Should have a default ticket price of 0.01 eth", async function () {
+    expect(await this.lottery.getTicketPrice()).to.equal(ethers.utils.parseEther('0.01'));
+  });
+
+  it("Should allow the ticket price to be changed to 0.02", async function () {
+    const setTicketPriceTx = await this.lottery.setTicketPrice(ethers.utils.parseEther('0.02'));
+    await setTicketPriceTx.wait();
+  });
+
+  it("Should have a new ticket price of 0.02 eth", async function () {
+    expect(await this.lottery.getTicketPrice()).to.equal(ethers.utils.parseEther('0.02'));
+  });
+
+  it("Should not be able to run prize draw with zero tickets", async function () {
+    await expect(this.lottery.startDraw()).to.be.revertedWith('There needs to be at least 10 players to run a draw');
+  });
+
+  it("Should not allow a ticket to be purchased for more than the ticket price", async function () {
+    await expect(this.lottery.purchaseTicket({value: ethers.utils.parseEther('0.03')})).to.be.revertedWith('You must pay the ticket price');
+  });
+
+  it("Should not allow a ticket to be purchased for less than the ticket price", async function () {
+    await expect(this.lottery.purchaseTicket({value: ethers.utils.parseEther('0.001')})).to.be.revertedWith('You must pay the ticket price');
+  });
+
+  it("Should allow the ticket to be purchased for 0.02 eth", async function () {
+    const setTicketPriceTx = await this.lottery.purchaseTicket({value: ethers.utils.parseEther('0.02')});
     await setTicketPriceTx.wait();
 
-    expect(await lottery.getTicketPrice()).to.equal("0.02");
+    expect(await this.lottery.getTickets()).lengthOf(1);
+
   });
+
 });
