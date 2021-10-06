3f7b7fa85bc3e5b71929824f4ff0bd10cc1245ed
Matthew Lanham <matt@geckoengage.com>
2021-10-06 19:59:56 +0100

    consistency

--- src/components/TicketPurchased.js
+++ src/components/TicketPurchased.js
@@ -1,7 +1,4 @@
-import React from "react";
-
-const TicketPurchased = (props) => {
-
+export default function TicketPurchased({ props }) {
     return (
         <div aria-live="assertive" className="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start">
             <div className="w-full flex flex-col items-center space-y-4 sm:items-end">
@@ -37,6 +34,4 @@ const TicketPurchased = (props) => {
             </div>
         </div>
     );
-}
-
-export default TicketPurchased;
+};
